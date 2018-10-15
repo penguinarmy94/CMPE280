3fb633eb0216ac65831491303e35900ea1c3496c
penguinarmy94 <luisote94@gmail.com>
2018-10-14 20:05:48 -0700

    Added prototype of AirSafe

--- AirQualityApp/views.py
+++ AirQualityApp/views.py
@@ -9,11 +9,11 @@ def index(request):
     return render(request, 'index.html', {'cities': cities, 'lat': 0, 'lng': 0})
 
 def latest(request):
-    location = geolite2.lookup(socket.gethostbyname(socket.gethostname()))# geocoder.ipinfo('me').latlng
-    print(location)
-    result = requests.get("https://api.openaq.org/v1/latest?coordinates=" + str(location[0]) + "," + str(location[1]) + "&radius=50", timeout=8)
+    #location = geolite2.lookup(socket.gethostbyname(socket.gethostname()))# geocoder.ipinfo('me').latlng
+    #print(location)
+    result = requests.get("https://api.openaq.org/v1/latest?coordinates=" + str(-30) + "," + str(-17.5) + "&radius=50", timeout=8)
     print(result.json())
     info = result.json()['results']
 
-    return render(request, 'index.html', {'cities': info, 'lat': location[0], 'lng': location[1]})
+    return render(request, 'index.html', {'cities': info, 'lat': -30, 'lng': -17.5})
     #result = requests.get("https://api.openaq.org/v1/latest")
