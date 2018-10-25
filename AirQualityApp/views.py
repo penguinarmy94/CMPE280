e6a9fd3c383a0a08c3a1024b68b3c2f76eec69e8
penguinarmy94 <luisote94@gmail.com>
2018-10-25 00:41:04 -0700

    Added html skeleton for home route

--- AirQualityApp/views.py
+++ AirQualityApp/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
-import requests, socket
-from geoip import geolite2
+#import requests, socket
+#from geoip import geolite2
 
 # Create your views here.
 def index(request):
+    """
     result = requests.get("https://api.openaq.org/v1/cities?limit=1", timeout=8)
     cities = result.json()['results']
-    return render(request, 'index.html', {'cities': cities, 'lat': 0, 'lng': 0})
+    """
+    return render(request, 'app.html', {'cities': "cities", 'lat': 0, 'lng': 0})
 
 def latest(request):
-    #location = geolite2.lookup(socket.gethostbyname(socket.gethostname()))# geocoder.ipinfo('me').latlng
-    #print(location)
+    """
+    location = geolite2.lookup(socket.gethostbyname(socket.gethostname()))# geocoder.ipinfo('me').latlng
+    print(location)
     result = requests.get("https://api.openaq.org/v1/latest?coordinates=" + str(-30) + "," + str(-17.5) + "&radius=50", timeout=8)
     print(result.json())
     info = result.json()['results']
+    """
 
-    return render(request, 'index.html', {'cities': info, 'lat': -30, 'lng': -17.5})
+    return render(request, 'index.html', {'cities': "", 'lat': -30, 'lng': -17.5})
     #result = requests.get("https://api.openaq.org/v1/latest")
