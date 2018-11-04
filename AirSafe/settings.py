76433608ec3590863930b185782479a049d672ad
penguinarmy94 <luisote94@gmail.com>
2018-11-04 14:31:45 -0800

    Virtual Environment Added

--- AirSafe/settings.py
+++ AirSafe/settings.py
@@ -75,11 +75,23 @@ WSGI_APPLICATION = 'AirSafe.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'airsafe',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'airnow.cq2wcl14nou2.us-west-1.rds.amazonaws.com',
+        'PORT': '3306'
+    }
+}
+
+"""
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
