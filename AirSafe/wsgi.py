eb202e4c7bb89a88412e4c7e0a6da41f6897a6ae
penguinarmy94 <luisote94@gmail.com>
2018-11-11 01:01:45 -0800

    added thread for daily update

--- AirSafe/wsgi.py
+++ AirSafe/wsgi.py
@@ -7,10 +7,33 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
-import os
+import os, threading, datetime
 
 from django.core.wsgi import get_wsgi_application
+from AirQualityApp import views
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AirSafe.settings")
 
 application = get_wsgi_application()
+
+def update():
+    next_date = datetime.date.today() + datetime.timedelta(days=1)
+    count = 0
+
+    while 1:
+        time = datetime.datetime.today().hour
+        day = datetime.date.today()
+
+        if time == 16:
+            if day == next_date:
+                views.dayUpdate()
+                next_date = day + datetime.timedelta(days=1)
+        """
+        if time > 0 and count%10000 == 0:
+            print("Day update in progress")
+        
+        count += 1"""
+    
+update_thread = threading.Thread(target=update)
+
+update_thread.start()
