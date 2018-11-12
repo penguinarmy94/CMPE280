a9dba4dfe81ad51666bc08564d57544b04f1a4ec
Yi-Chin Hsiao <yichinhsiao@Yi-Chins-MacBook-Pro.local>
2018-11-12 00:44:56 -0800

    Update sending email feature

--- AirQualityApp/cron.py
+++ AirQualityApp/cron.py
@@ -1,35 +1,30 @@
 import smtplib
+from . import models
 
 #send email
 def send_email():
-	sender = "garyhsiao1219@gmail.com"
-	receiver = "garyhsiao1219@gmail.com"
-	message = """From: AirSafe <airsafe@gmail.com>
-To: Yi-Chin Hsiao <garyhsiao1219@gmail.com>
+	users = models.User.objects.all();
+	for user in users:
+		aq = models.AQ.objects.filter(zipcode=user.zipcode).order_by("-stamp")[0]
+		sender = "cmpe280.airsafe@gmail.com"
+		receiver = user.email
+		message = """From: AirSafe <cmpe280.airsafe@gmail.com>
+To: """ + user.email + """
 Subject: AirSafe Test Email
 
-This is a test email from AirSafe.
-"""
+City: """ + aq.city + """    State: """ + aq.state + """
+Date: """ + str(aq.stamp) + """
+Ozone(O3) AQI: """ + str(aq.ozone) + """
+PM2.5 AQI: """ + str(aq.pm)
 
-	try:
-		server = smtplib.SMTP("smtp.gmail.com", 587)
-		server.ehlo()
-		server.starttls()
-		server.ehlo()
-		server.login("garyhsiao1219@gmail.com", "yichin0091")
-		server.sendmail(sender, receiver, message)
-		server.close()
-		print ("Email sent successfully")
-	except smtplib.SMTPException:
-		print ("Error: Unable to send the email")
-		
-# import pymysql
-# 
-# conn = pymysql.connect(host='airnow.cq2wcl14nou2.us-west-1.rds.amazonaws.com', 
-# 					   user='root',password='password',db='airsafe')
-# cur = conn.cursor()
-# sql = "SELECT email, zipcode FROM airsafe.User;"
-# cur.execute(sql)
-# users = cur.fetchall()
-# for user in users:
-# 	print("{0} {1}".format(user[0], user[1]))
+		try:
+			server = smtplib.SMTP("smtp.gmail.com", 587)
+			server.ehlo()
+			server.starttls()
+			server.ehlo()
+			server.login("cmpe280.airsafe@gmail.com", "airsafe280")
+			server.sendmail(sender, receiver, message)
+			server.close()
+			print ("Email sent successfully")
+		except smtplib.SMTPException:
+			print ("Error: Unable to send the email")
