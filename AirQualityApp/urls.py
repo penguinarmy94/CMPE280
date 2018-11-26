630caaeed0ef6fe74714467eba060fdae1d10ea7
penguinarmy94 <luisote94@gmail.com>
2018-11-25 19:21:38 -0800

    Static file update, logo and loading images loaded

--- AirQualityApp/urls.py
+++ AirQualityApp/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -10,4 +12,4 @@ urlpatterns = [
     path('GetData', views.GetPastData, name='GetData'),
     path('verifyEmail', views.verifyEmail, name='verifyEmail'),
     path('subscription', views.subscription, name='subscription')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
